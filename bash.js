5a726bad609d251da32fd6c520eae91be0a7b2bd
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-06-17 12:45:29 -0400

    add echo

--- bash.js
+++ bash.js
@@ -1,15 +1,8 @@
 process.stdout.write('prompt > ');
-
+const commands = require('./command');
 process.stdin.on('data', function(data) {
   let cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(process.argv[1]);
-  } else if (cmd === 'date') {
-    let dateString = new Date();
-    dateString = dateString.toString();
-    process.stdout.write(dateString);
-  } else {
-    process.stdout.write('You typed: ' + cmd);
+  if (commands[cmd]) {
+    commands[cmd]();
   }
-  process.stdout.write('\nprompt > ');
 });
